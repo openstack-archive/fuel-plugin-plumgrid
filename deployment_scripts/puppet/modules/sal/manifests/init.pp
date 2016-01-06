d8c4154491fa89a289c183f64a3d3ec19fed0998
Javeria Khan <javeriak@plumgrid.com>
2016-01-06 11:20:43 -0800

    Initial Commit - Fuel PLUMgrid Plugin
    - Supports MOS 7.0 deployment
    
    Change-Id: I3f6a17ba3b8720e22d3d1be29e45b13077879210
    Signed-off-by: Javeria Khan <javeriak@plumgrid.com>

---
+++ deployment_scripts/puppet/modules/sal/manifests/init.pp
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2016, PLUMgrid Inc, http://plumgrid.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class sal ($plumgrid_ip = '',
+           $virtual_ip = '',
+           $rest_port = '9180',
+           $mgmt_dev = '%AUTO_DEV%',
+           ) {
+  $lxc_root_path = '/var/lib/libvirt/filesystems/plumgrid'
+  $lxc_data_path = '/var/lib/libvirt/filesystems/plumgrid-data'
+
+  firewall { '001 allow PG Console access':
+    destination => $virtual_ip,
+    dport  => 443,
+    proto  => tcp,
+    action => accept,
+    before => [ Class['sal::nginx'], Class['sal::keepalived'] ],
+  }
+
+  class { 'sal::nginx':
+    plumgrid_ip => $plumgrid_ip,
+    virtual_ip => $virtual_ip,
+  }
+  class { 'sal::keepalived':
+    virtual_ip => $virtual_ip,
+    mgmt_dev => $mgmt_dev,
+  }
+}
