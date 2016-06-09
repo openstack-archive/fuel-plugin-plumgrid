86cfba5aa2e92d1345849d4a842e80425ae69257
Muhammad Shahzeb <mshahzeb@plumgrid.com>
2016-06-09 03:37:41 -0400

    Add support for MOS8 in fuel plumgrid plugin  - Changed parameters in environment config file  - Changed values in metadata file  - Changed director.pp manifest     - Remove neutron-plugin package     - Add support for deb based networking-plumgrid       package  - Changed core plugin value  - Replace plugin name in guess function  - Add iptables rule for metadata  - Add support for solutions api (license, ips, zoneinfo)  - Fixed cleanup script  - Delete all neutron agents at deployment  - Place nova sql url in nova.conf of compute  - Add fabric network support in modules
    Ticket: [SOL-1022] [SOL-975] [SOL-782]
    Change-Id: I4fdf75de95d367c9ae197d384ebc2eb1e4df738a
    Signed-off-by: Muhammad Shahzeb <mshahzeb@plumgrid.com>
    (cherry picked from commit d28a0b75def94237656380f00ece7c24110df0e2)
    (cherry picked from commit 2bfe4490a78b1e422b999d1655b0cb9015fdc239)

--- deployment_scripts/puppet/manifests/pre_deployment.pp
+++ deployment_scripts/puppet/manifests/pre_deployment.pp
@@ -23,5 +23,13 @@ package { 'libvirt-bin' :
 }
 package { 'networking-plumgrid':
   ensure   => 'absent',
-  provider => 'pip',
+}
+
+# MOS8 was tagged with the older version of puppet neutron which contains outdated PLUMgrid plugin name 
+
+file_line { 'Replace outdated plugin package name in puppet neutron':
+   path     => '/etc/puppet/modules/neutron/manifests/params.pp',
+   line     => "    \$plumgrid_plugin_package    = \'networking-plumgrid\'",
+   match    => "plumgrid_plugin_package",
+   multiple => true
 }
